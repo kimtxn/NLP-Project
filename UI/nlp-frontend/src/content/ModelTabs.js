9d361a91427ce9d4f44a29d98ccfa3c89178a7d8
Shun Bin <shunbin.yeoh@hubble.sg>
2022-11-05 01:52:07 +0800

    api bug

--- UI/nlp-frontend/src/content/ModelTabs.js
+++ UI/nlp-frontend/src/content/ModelTabs.js
@@ -5,8 +5,9 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Example from './Example.js'
-import { TextField } from '@mui/material';
+import { TextareaAutosize, TextField } from '@mui/material';
 import { Button } from '@mui/material';
+import fetch from "node-fetch";
 import './Content.css'
 
 export default function ModelTabs() {
@@ -15,18 +16,85 @@ export default function ModelTabs() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  
+  const [input, setInput] = React.useState();
 
+  const [output, setOutput] = React.useState();
+
+  async function query(data) {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/LYTinn/finetuning-sentiment-model-tweet-bert",
+      {
+        headers: { Authorization: "Bearer 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" },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    setOutput(result);
+    return result;
+  }
+  console.log();
+ 
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Demo" value="1" />
-            <Tab label="Model Card" value="2" />
-            <Tab label="Model Usage" value="3" />
+            <Tab label="BERT" value="1" />
+            <Tab label="GPT2" value="2" />
+            <Tab label="Bloom" value="3" />
+            <Tab label="3in1" value="4" />
           </TabList>
         </Box>
         <TabPanel value="1">
+          <h4>Sample Inputs</h4>
+          <Example/>
+          <h4>Enter your own input</h4>
+          <div className="inputs">
+            <TextField id="input_text" variant="outlined" 
+            value={input} onChange={(e) => setInput(e.target.value)}/>
+          </div>
+          <br></br>
+          <button id="model" onClick={query}>
+              Get Sentiment
+          </button>
+          <br></br>
+          <h4>Result</h4>
+          <div className="outputs">
+            <TextField id="outlined-read-only-input" variant="outlined" value={output} InputProps={{
+            readOnly: true,
+          }}/>
+          </div>
+          <br></br>
+          <h4>Model Card</h4>
+          Further info of model
+          <br></br><br></br>
+          <h4>Model Usage</h4>
+          How to download info
+        </TabPanel>
+
+
+        <TabPanel value="2">
+          <h4>Sample Inputs</h4>
+          <Example/>
+          <h4>Enter your own input</h4>
+          <div className="inputs">
+            <TextField id="outlined-basic" variant="outlined" />
+          </div>
+          <br></br>
+          <Button id="model">
+              Get Sentiment 
+          </Button>
+          <br></br><br></br>
+          <h4>Model Card</h4>
+          Further info of model
+          <br></br><br></br>
+          <h4>Model Usage</h4>
+          How to download info
+        </TabPanel>
+
+        <TabPanel value="3">
           <h4>Sample Inputs</h4>
           <Example/>
           <h4>Enter your own input</h4>
@@ -37,9 +105,34 @@ export default function ModelTabs() {
           <Button id="model">
               Run Model
           </Button>
+          <br></br><br></br>
+          <h4>Model Card</h4>
+          Further info of model
+          <br></br><br></br>
+          <h4>Model Usage</h4>
+          How to download info
         </TabPanel>
-        <TabPanel value="2">Content Two</TabPanel>
-        <TabPanel value="3">Content Three</TabPanel>
+
+        <TabPanel value="4">
+          <h4>Sample Inputs</h4>
+          <Example/>
+          <h4>Enter your own input</h4>
+          <div className="inputs">
+            <TextField id="outlined-basic" variant="outlined" />
+          </div>
+          <br></br>
+          <Button id="model">
+              Run Model
+          </Button>
+          <br></br><br></br>
+          <h4>Model Card</h4>
+          Further info of model
+          <br></br><br></br>
+          <h4>Model Usage</h4>
+          How to download info
+        </TabPanel>
+
+        
       </TabContext>
     </Box>
   );
